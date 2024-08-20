536638793bddefad5474c738e52ba03a7f950e76
Pipsisewah <stevenanthonylopes@gmail.com>
2024-08-20 10:19:27 -0500

    idor adding initial tests

--- idor/containers/targetMachine/index.js
+++ idor/containers/targetMachine/index.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
+
+app.use(express.json());
 
 // Sample in-memory database
 const users = [
@@ -10,16 +13,8 @@ const users = [
     { id: 2, name: 'Bob', role: 'user', secret: 'User Secret' },
 ];
 
-// Middleware to simulate authentication
-app.use((req, res, next) => {
-    // In a real app, you'd authenticate users with a token or session.
-    // For simplicity, we'll assume the user is always "Bob" with user ID 2.
-    req.user = users[1]; // Bob
-    next();
-});
-
 // Vulnerable endpoint
-app.get('/user/:id/secret', (req, res) => {
+app.get('/user/:id/', (req, res) => {
     const userId = parseInt(req.params.id, 10);
     const user = users.find(u => u.id === userId);
 
@@ -28,8 +23,38 @@ app.get('/user/:id/secret', (req, res) => {
     }
 
     // Here is the IDOR: It allows access to any user's secret based on the provided ID.
-    res.json({ secret: user.secret });
+    res.json({ user });
 });
+
+/**
+ * IDOR updates the profile of whatever userId is provided
+ */
+app.post('/updateProfile', (req, res) => {
+    const { userId, newProfileData } = req.body;
+    const user = users.find(u => u.id === userId);
+    user.profile = newProfileData;
+    res.json({ message: 'Profile updated' });
+});
+
+/**
+ * IDOR provides access to get user info based on a header
+ */
+app.get('/getUserData', (req, res) => {
+    const userId = parseInt(req.headers['x-user-id'], 10);
+    const user = users.find(u => u.id === userId);
+    res.json({user});
+});
+
+/**
+ * IDOR allows access to any file requested
+ */
+app.get('/files/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, 'files', filename);
+    console.log(`filePath: ${filePath}`);
+    res.sendFile(filePath);
+});
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
